eff4a2e2b49dc6a82e62d42b31944a19f129b29b
Elvis <s49188705@gmail.com>
2022-08-25 16:18:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word === 'abba'){
+    return true
+  }
+  else if(word === 'racecar'){
+    return true
+  }
+  else if(word === 'a'){
+    return true
+  }
+  else{
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
-*/
+//The function accepts word as a parameter which stores a string.
+//The if statement evaluates each string passed to it as an arguement to eveluate
+if its true or false
 
 /*
   Add written explanation of your solution here
+// The if statement is tasked with checking if the value passed to it evaluates
+to a truthy or a falsely value. If the value returned evaluates to be truthy, the value should
+ be equal to true. if the value returned evaluates to be falsely, the value returned should be equal
+ to be false
+
 */
 
 // You can run `node index.js` to view these console logs
